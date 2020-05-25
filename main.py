b9d47767576de46e7b1b2648fb2e5a76f816b474
victorvermot <48206917+victorvermot@users.noreply.github.com>
2020-05-25 19:17:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ ORIGINAL_TEXTES_PATH = "./textes"
 GENERATED_TEXTES_PATH = "./generated_json"
 
 def main():
-    return
 
+    book_analyzor = Book()
 
+    for filename in os.listdir(ORIGINAL_TEXTES_PATH):
+        if not filename.endswith(".txt"):
+            continue
 
-
-
+        with open(os.path.join(GENERATED_TEXTES_PATH, filename), 'r', encoding='utf-8') as file:
+            content = file.read()
+        
+        book = book_analyzor.???
 
 if __name__ == "__main__":
     main()
